483e1a6fb66086e55bc10e09d7e0425f8d016322
katchen1 <katherinechen139@gmail.com>
2021-07-12 10:49:55 -0700

    User can see a list of entries in a card-based layout in the home fragment

---
+++ app/src/main/java/com/example/myanimereport/adapters/EntriesAdapter.java
@@ -0,0 +1,66 @@
+package com.example.myanimereport.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.example.myanimereport.databinding.ItemEntryBinding;
+import com.example.myanimereport.models.Anime;
+import com.example.myanimereport.models.Entry;
+import java.util.List;
+import java.util.Locale;
+
+public class EntriesAdapter extends RecyclerView.Adapter<EntriesAdapter.ViewHolder> {
+
+    private final Context context;
+    private final List<Entry> entries;
+
+    /* Constructor takes the context and the list of entries in the recycler view. */
+    public EntriesAdapter(Context context, List<Entry> entries) {
+        this.context = context;
+        this.entries = entries;
+    }
+
+    /* Creates a view holder for the entry. */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(ItemEntryBinding.inflate(LayoutInflater.from(context)));
+    }
+
+    /* Binds the entry at the passed in position to the view holder. */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Entry entry = entries.get(position);
+        holder.bind(entry);
+    }
+
+    /* Returns the number of entries in the list. */
+    @Override
+    public int getItemCount() {
+        return entries.size();
+    }
+
+    /* Defines the view holder for a entry. */
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final ItemEntryBinding binding;
+
+        /* Constructor takes in a binding for the entry's view. */
+        public ViewHolder(ItemEntryBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        /* Binds the entry's data to the view's components. */
+        public void bind(Entry entry) {
+            Anime anime = entry.getAnime();
+            Glide.with(context).load(anime.getBannerImage()).into(binding.ivImage);
+            binding.tvTitle.setText(anime.getTitle());
+            binding.tvYearWatched.setText(String.format(Locale.getDefault(), "%d", entry.getYearWatched()));
+            binding.tvRating.setText(String.format(Locale.getDefault(), "%.1f", entry.getRating()));
+        }
+    }
+}
