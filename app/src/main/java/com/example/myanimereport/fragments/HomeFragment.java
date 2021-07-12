e9eb6ac503019ec8083ba429a6b4f037c7cd2463
katchen1 <katherinechen139@gmail.com>
2021-07-12 10:49:55 -0700

    User can see a list of entries in a card-based layout in the home fragment

--- app/src/main/java/com/example/myanimereport/fragments/HomeFragment.java
+++ app/src/main/java/com/example/myanimereport/fragments/HomeFragment.java
@@ -7,12 +7,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import com.example.myanimereport.R;
+import androidx.recyclerview.widget.GridLayoutManager;
+import com.example.myanimereport.adapters.EntriesAdapter;
 import com.example.myanimereport.databinding.FragmentHomeBinding;
+import com.example.myanimereport.models.Entry;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private List<Entry> entries;
+    private EntriesAdapter adapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -24,7 +30,17 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.textHome.setText(R.string.title_home);
+
+        // Set up adapter and layout of recycler view
+        entries = new ArrayList<>();
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        adapter = new EntriesAdapter(getContext(), entries);
+        binding.rvEntries.setLayoutManager(layoutManager);
+        binding.rvEntries.setAdapter(adapter);
+
+        // Add placeholder entries
+        for (int i = 0; i < 11; i++) entries.add(new Entry());
+        adapter.notifyDataSetChanged();
     }
 
     @Override
