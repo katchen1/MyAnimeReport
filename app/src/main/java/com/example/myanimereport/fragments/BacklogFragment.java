6c15d9cdae6bec7d880daf17f19385294c622571
katchen1 <katherinechen139@gmail.com>
2021-07-12 11:26:00 -0700

    User can view a list of backlog items

--- app/src/main/java/com/example/myanimereport/fragments/BacklogFragment.java
+++ app/src/main/java/com/example/myanimereport/fragments/BacklogFragment.java
@@ -6,13 +6,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import com.example.myanimereport.R;
+import com.example.myanimereport.adapters.BacklogItemsAdapter;
 import com.example.myanimereport.databinding.FragmentBacklogBinding;
+import com.example.myanimereport.models.BacklogItem;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class BacklogFragment extends Fragment {
 
     private FragmentBacklogBinding binding;
+    private List<BacklogItem> items;
+    private BacklogItemsAdapter adapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -24,7 +34,22 @@ public class BacklogFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.textBacklog.setText(R.string.title_backlog);
+
+        // Set up adapter and layout of recycler view
+        items = new ArrayList<>();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(requireContext());
+        adapter = new BacklogItemsAdapter(getContext(), items);
+        binding.rvBacklogItems.setLayoutManager(layoutManager);
+        binding.rvBacklogItems.setAdapter(adapter);
+
+        // Divider between items
+        DividerItemDecoration divider = new DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL);
+        divider.setDrawable(Objects.requireNonNull(ContextCompat.getDrawable(requireContext(), R.drawable.item_divider)));
+        binding.rvBacklogItems.addItemDecoration(divider);
+
+        // Add placeholder items
+        for (int i = 0; i < 30; i++) items.add(new BacklogItem());
+        adapter.notifyDataSetChanged();
     }
 
     @Override
