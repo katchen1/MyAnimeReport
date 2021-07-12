9ef956d05a19d282d2c9404ed1ec169d210b0dd7
katchen1 <katherinechen139@gmail.com>
2021-07-12 14:03:46 -0700

    User can log in

--- app/src/main/java/com/example/myanimereport/models/Entry.java
+++ app/src/main/java/com/example/myanimereport/models/Entry.java
@@ -1,43 +1,78 @@
 package com.example.myanimereport.models;
 
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
 /* Entry (Parse model). */
-public class Entry {
-    Integer mediaId; //	The AniList mediaId of the anime watched
-    Integer monthWatched; // The month when the user watched the anime
-    Integer yearWatched; // The year when the user watched the anime
-    Double rating; // The user’s rating of the anime (out of 10)
-    String note; // An optional reflective note on the anime
+@ParseClassName("Entry")
+public class Entry extends ParseObject {
+    public static final String KEY_USER = "user";
+    public static final String KEY_MEDIA_ID = "mediaId";
+    public static final String KEY_MONTH_WATCHED = "monthWatched";
+    public static final String KEY_YEAR_WATCHED = "yearWatched";
+    public static final String KEY_RATING = "rating";
+    public static final String KEY_NOTE = "note";
 
     public Entry() {
-        mediaId = 155;
-        monthWatched = 1;
-        yearWatched = 2012;
-        rating = 10.0;
-        note = "My favorite anime!";
+        setUser(ParseUser.getCurrentUser());
+        setMediaId(155); // The AniList mediaId of the anime watched
+        setMonthWatched(1); // The month when the user watched the anime
+        setYearWatched(2012); // The year when the user watched the anime
+        setRating(10.0); // The user’s rating of the anime (out of 10)
+        setNote("My favorite anime!"); // An optional reflective note on the anime
     }
 
-    /* Getters. */
-    public Anime getAnime() {
-        return new Anime(); // Change this to query the anime with mediaId
+    /* Getters and setters. */
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user) {
+        put(KEY_USER, user);
     }
 
     public Integer getMediaId() {
-        return mediaId;
+        return getInt(KEY_MEDIA_ID);
+    }
+
+    public void setMediaId(Integer mediaId) {
+        put(KEY_MEDIA_ID, mediaId);
     }
 
     public Integer getMonthWatched() {
-        return monthWatched;
+        return getInt(KEY_MONTH_WATCHED);
+    }
+
+    public void setMonthWatched(Integer monthWatched) {
+        put(KEY_MONTH_WATCHED, monthWatched);
     }
 
     public Integer getYearWatched() {
-        return yearWatched;
+        return getInt(KEY_YEAR_WATCHED);
+    }
+
+    public void setYearWatched(Integer monthWatched) {
+        put(KEY_YEAR_WATCHED, monthWatched);
     }
 
     public Double getRating() {
-        return rating;
+        return getDouble(KEY_RATING);
+    }
+
+    public void setRating(Double rating) {
+        put(KEY_RATING, rating);
     }
 
     public String getNote() {
-        return note;
+        return getString(KEY_NOTE);
+    }
+
+    public void setNote(String note) {
+        put(KEY_NOTE, note);
+    }
+
+    public Anime getAnime() {
+        return new Anime();
     }
 }
