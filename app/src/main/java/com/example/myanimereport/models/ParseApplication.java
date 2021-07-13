4945f0cc7fc23fed274fe3f7f0a581b739b632c1
katchen1 <katherinechen139@gmail.com>
2021-07-13 08:26:07 -0700

    Set up Apollo client for GraphQL

--- app/src/main/java/com/example/myanimereport/models/ParseApplication.java
+++ app/src/main/java/com/example/myanimereport/models/ParseApplication.java
@@ -1,10 +1,20 @@
 package com.example.myanimereport.models;
 
 import android.app.Application;
+import android.util.Log;
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.example.MediaDetailsByTitleQuery;
 import com.parse.Parse;
 import com.parse.ParseObject;
+import org.jetbrains.annotations.NotNull;
 
 public class ParseApplication extends Application {
+
+    public static ApolloClient apolloClient;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -19,6 +29,24 @@ public class ParseApplication extends Application {
                 .clientKey("twJERNUXhCGFVxzruL5KfoWwlGjgsKLFYlgS8ugJ")
                 .server("https://parseapi.back4app.com")
                 .build()
+        );
+
+        // Initialize the Apollo client
+        apolloClient = ApolloClient.builder().serverUrl("https://graphql.anilist.co/post").build();
+
+        // Run a simple test
+        apolloClient.query(new MediaDetailsByTitleQuery("conan")).enqueue(
+            new ApolloCall.Callback<MediaDetailsByTitleQuery.Data>() {
+                @Override
+                public void onResponse(@NotNull Response<MediaDetailsByTitleQuery.Data> response) {
+                    Log.i("Apollo", "Response data: " + response.getData().toString());
+                }
+
+                @Override
+                public void onFailure(@NotNull ApolloException e) {
+                    Log.i("Apollo", "Error: ", e);
+                }
+            }
         );
     }
 }
