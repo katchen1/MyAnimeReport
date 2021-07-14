7b6807d4f704e9899fb4c0f642020c13ff88afc1
katchen1 <katherinechen139@gmail.com>
2021-07-14 09:34:49 -0700

    User can edit an entry

--- app/src/main/java/com/example/myanimereport/activities/EntryDetailsActivity.java
+++ app/src/main/java/com/example/myanimereport/activities/EntryDetailsActivity.java
@@ -3,9 +3,14 @@ package com.example.myanimereport.activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
@@ -24,6 +29,8 @@ public class EntryDetailsActivity extends AppCompatActivity {
     private ActivityEntryDetailsBinding binding;
     private Entry entry; // The entry whose information is being shown
     private Integer position; // The position of the entry in the adapter
+    private Anime anime; // The anime of the entry
+    public static final int EDIT_ENTRY_REQUEST_CODE = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,12 @@ public class EntryDetailsActivity extends AppCompatActivity {
         position = getIntent().getIntExtra("position", -1);
 
         // Show the entry's information
+        populateEntryView();
+    }
+
+    /* Shows the entry's information. */
+    public void populateEntryView() {
+        // Data unrelated to the anime
         String month = (new DateFormatSymbols().getMonths()[entry.getMonthWatched() - 1]);
         binding.tvMonthWatched.setText(month);
         binding.tvYearWatched.setText(String.format(Locale.getDefault(), "%d", entry.getYearWatched()));
@@ -48,23 +61,48 @@ public class EntryDetailsActivity extends AppCompatActivity {
         // Make a query for the anime's cover image and title
         Integer mediaId = entry.getMediaId();
         ParseApplication.apolloClient.query(new MediaDetailsByIdQuery(mediaId)).enqueue(
-            new ApolloCall.Callback<MediaDetailsByIdQuery.Data>() {
-                @Override
-                public void onResponse(@NonNull Response<MediaDetailsByIdQuery.Data> response) {
-                    // View editing needs to happen in the main thread, not the background thread
-                    ParseApplication.currentActivity.runOnUiThread(() -> {
-                        Anime anime = new Anime(response);
-                        Glide.with(EntryDetailsActivity.this).load(anime.getCoverImage()).into(binding.ivImage);
-                        binding.tvTitle.setText(anime.getTitleEnglish());
-                        binding.cvAnime.setStrokeColor(anime.getColor());
-                    });
-                }
+                new ApolloCall.Callback<MediaDetailsByIdQuery.Data>() {
+                    @Override
+                    public void onResponse(@NonNull Response<MediaDetailsByIdQuery.Data> response) {
+                        // View editing needs to happen in the main thread, not the background thread
+                        ParseApplication.currentActivity.runOnUiThread(() -> {
+                            anime = new Anime(response);
+                            Glide.with(EntryDetailsActivity.this).load(anime.getCoverImage()).into(binding.ivImage);
+                            binding.tvTitle.setText(anime.getTitleEnglish());
+                            binding.cvAnime.setStrokeColor(anime.getColor());
+                        });
+                    }
 
-                @Override
-                public void onFailure(@NonNull ApolloException e) {
-                    Log.e(TAG, e.getMessage());
+                    @Override
+                    public void onFailure(@NonNull ApolloException e) {
+                        Log.e(TAG, e.getMessage());
+                    }
                 }
-            }
         );
+    }
+
+    public void btnInfoOnClick(View view) {
+        Toast.makeText(this, "Not implemented", Toast.LENGTH_SHORT).show();
+    }
+
+    public void btnEditOnClick(View view) {
+        Intent intent = new Intent(EntryDetailsActivity.this, EntryActivity.class);
+        intent.putExtra("entry", entry); // Pass in the entry to edit
+        intent.putExtra("title", anime.getTitleEnglish()); // Also pass in the title to reduce queries
+        startActivityForResult(intent, EDIT_ENTRY_REQUEST_CODE);
+    }
+
+    public void btnDeleteOnClick(View view) {
+        Toast.makeText(this, "Not implemented", Toast.LENGTH_SHORT).show();
+    }
+
+    /* After returning from a entry edit activity, update the entry. */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == EDIT_ENTRY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            entry = data.getParcelableExtra("entry");
+            populateEntryView();
+        }
     }
 }
