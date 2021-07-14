5700d78e8d0519f9e289c7226952c803d7f67333
katchen1 <katherinechen139@gmail.com>
2021-07-13 21:50:45 -0700

    Prevent destroy of fragment when switching between tabs to reduce excessive network requests

--- app/src/main/java/com/example/myanimereport/activities/MainActivity.java
+++ app/src/main/java/com/example/myanimereport/activities/MainActivity.java
@@ -3,27 +3,66 @@ package com.example.myanimereport.activities;
 import android.os.Bundle;
 import com.example.myanimereport.R;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import com.example.myanimereport.databinding.ActivityMainBinding;
+import com.example.myanimereport.fragments.BacklogFragment;
+import com.example.myanimereport.fragments.HomeFragment;
+import com.example.myanimereport.fragments.MatchFragment;
+import com.example.myanimereport.fragments.ReportFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // Hide action bar
         if (getSupportActionBar() != null) getSupportActionBar().hide();
 
-        // Set up navigation controller
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        // Set up fragments
+        final FragmentManager manager = getSupportFragmentManager();
+        final Fragment homeFragment = new HomeFragment();
+        final Fragment reportFragment = new ReportFragment();
+        final Fragment matchFragment = new MatchFragment();
+        final Fragment backlogFragment = new BacklogFragment();
+
+        // Use a one-element array to store the active fragment to make it effectively final
+        final Fragment[] activeFragment = { homeFragment };
+
+        // Add fragments to the fragment manager
+        manager.beginTransaction()
+                .add(R.id.contentMain, backlogFragment, "backlog").hide(backlogFragment)
+                .add(R.id.contentMain, matchFragment, "match").hide(matchFragment)
+                .add(R.id.contentMain, reportFragment, "report").hide(reportFragment)
+                .add(R.id.contentMain, homeFragment, "home").commit();
+
+        // Handle tab changes
+        binding.navView.setOnNavigationItemSelectedListener(item -> {
+            // Get the target fragment
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                default:
+                    fragment = homeFragment;
+                    break;
+                case R.id.navigation_report:
+                    fragment = reportFragment;
+                    break;
+                case R.id.navigation_match:
+                    fragment = matchFragment;
+                    break;
+                case R.id.navigation_backlog:
+                    fragment = backlogFragment;
+                    break;
+            }
+
+            // Use hide()+show() instead of replace() to prevent the fragments from being destroyed
+            manager.beginTransaction().hide(activeFragment[0]).show(fragment).commit();
+            activeFragment[0] = fragment;
+            return true;
+        });
     }
 }
