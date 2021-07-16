5348dfb1c94598350d3f2b6ab81704d3cb57d665
katchen1 <katherinechen139@gmail.com>
2021-07-16 11:27:10 -0700

    Shows horizontal bar chart for genre preference

--- app/src/main/java/com/example/myanimereport/utils/CustomMarkerView.java
+++ app/src/main/java/com/example/myanimereport/utils/CustomMarkerView.java
@@ -5,8 +5,10 @@ import android.widget.TextView;
 
 import com.github.mikephil.charting.components.IMarker;
 import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.CandleEntry;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.example.myanimereport.R;
 import com.github.mikephil.charting.utils.MPPointF;
@@ -16,12 +18,13 @@ public class CustomMarkerView extends MarkerView implements IMarker {
 
     private TextView tvXContent;
     private TextView tvYContent;
+    private int mode;
 
-    public CustomMarkerView (Context context, int layoutResource) {
+    public CustomMarkerView (Context context, int layoutResource, int mode) {
         super(context, layoutResource);
         tvXContent = (TextView) findViewById(R.id.tvXContent);
         tvYContent = (TextView) findViewById(R.id.tvYContent);
-
+        this.mode = mode;
     }
 
     // callbacks everytime the MarkerView is redrawn, can be used to update the
@@ -32,8 +35,17 @@ public class CustomMarkerView extends MarkerView implements IMarker {
             CandleEntry ce = (CandleEntry) e;
             tvYContent.setText("" + Utils.formatNumber(ce.getHigh(), 0, false));
         } else {
-            tvXContent.setText(Utils.formatNumber(e.getX(), 0, false));
-            tvYContent.setText("Animes Watched: " + Utils.formatNumber(e.getY(), 0, true));
+            if (mode == 0) tvXContent.setText(Utils.formatNumber(e.getX(), 0, false));
+            else if (mode == 1) tvXContent.setText(((PieEntry) e).getLabel());
+            else if (mode == 2) tvXContent.setText(((BarEntry) e).getData().toString());
+
+            String yStr = "";
+            if (mode == 0 || mode == 1) {
+                yStr = "Animes Watched: " + Utils.formatNumber(e.getY(), 0, true);
+            } else {
+                yStr = "Average Rating: " + e.getY();
+            }
+            tvYContent.setText(yStr);
         }
         super.refreshContent(e, highlight);
     }
