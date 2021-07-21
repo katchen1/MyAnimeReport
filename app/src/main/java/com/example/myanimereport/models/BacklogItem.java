3312b7786e2b7e695fee0ac9a2b55c56c97d81b2
katchen1 <katherinechen139@gmail.com>
2021-07-21 10:30:38 -0700

    Null checking across all files

--- app/src/main/java/com/example/myanimereport/models/BacklogItem.java
+++ app/src/main/java/com/example/myanimereport/models/BacklogItem.java
@@ -5,10 +5,13 @@ import androidx.annotation.NonNull;
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
+import com.example.MediaDetailsByIdListQuery;
 import com.example.MediaDetailsByIdQuery;
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+import java.util.ArrayList;
+import java.util.List;
 
 /* BacklogItem (Parse model). */
 @ParseClassName("BacklogItem")
@@ -27,19 +30,44 @@ public class BacklogItem extends ParseObject {
     }
 
     public void setAnime() {
-        System.out.println("SET ANIME");
         ParseApplication.apolloClient.query(new MediaDetailsByIdQuery(getMediaId())).enqueue(
-                new ApolloCall.Callback<MediaDetailsByIdQuery.Data>() {
-                    @Override
-                    public void onResponse(@NonNull Response<MediaDetailsByIdQuery.Data> response) {
-                        anime = new Anime(response);
-                    }
+            new ApolloCall.Callback<MediaDetailsByIdQuery.Data>() {
+                @Override
+                public void onResponse(@NonNull Response<MediaDetailsByIdQuery.Data> response) {
+                    anime = new Anime(response);
+                }
 
-                    @Override
-                    public void onFailure(@NonNull ApolloException e) {
-                        Log.e("Apollo", e.getMessage());
+                @Override
+                public void onFailure(@NonNull ApolloException e) {
+                    Log.e("Apollo", e.getMessage());
+                }
+            }
+        );
+    }
+
+    public static void setAnimes(List<BacklogItem> items) {
+        System.out.println("Set animes.");
+        List<Integer> ids = new ArrayList<>();
+        for (BacklogItem item: items) ids.add(item.getMediaId());
+        ParseApplication.apolloClient.query(new MediaDetailsByIdListQuery(1, ids)).enqueue(
+            new ApolloCall.Callback<MediaDetailsByIdListQuery.Data>() {
+                @Override
+                public void onResponse(@NonNull Response<MediaDetailsByIdListQuery.Data> response) {
+                    if (response.getData().Page() == null) return;
+                    if (response.getData().Page().media() == null) return;
+                    for (MediaDetailsByIdListQuery.Medium m: response.getData().Page().media()) {
+                        Anime anime = new Anime(m.fragments().mediaFragment());
+                        ParseApplication.seenMediaIds.add(anime.getMediaId());
+                        int index = ids.indexOf(anime.getMediaId());
+                        items.get(index).setAnime(anime);
                     }
                 }
+
+                @Override
+                public void onFailure(@NonNull ApolloException e) {
+                    Log.e("Apollo", e.getMessage() + e.getCause());
+                }
+            }
         );
     }
 
